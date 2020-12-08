be46edde79837645143295797d5acb1f15d1ae54
manojkumardesai <manojkumardbe@gmail.com>
2020-12-09 03:53:09 +0530

    conditional rendering based flags implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 // API
-import { fetchTriviaQuestions, Difficulty, QuizType } from './services/QUIZ_API';
+import { fetchTriviaQuestions, Difficulty, QuizType, QuestionExtended, AnswerObject } from './services/QUIZ_API';
 
 // Components
 import TriviaCard from './components/TriviaCard';
@@ -10,16 +10,29 @@ const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionExtended[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchTriviaQuestions(TOTAL_QUESTIONS, Difficulty.EASY, QuizType.MULTIPLE_CHOICE));
+  console.log(questions);
 
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchTriviaQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY,
+      QuizType.MULTIPLE_CHOICE
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -33,24 +46,31 @@ const App = () => {
   return (
     <div className="App">
       <h1>Trivia Game</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">
-        Score:
-      </p>
-      <p>Loading Questions...</p>
-      {/* <TriviaCard 
-        questionNumber = {number + 1}
-        totalQuestions = {TOTAL_QUESTIONS}
-        question = {questions[number].question}
-        answers = {questions[number].answers}
-        userAnswer = {userAnswers ? userAnswers[number]: undefined}
-        callback = {checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {
+        gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start Game
+        </button>
+        ) : null
+      }
+      { !gameOver ? <p className="score">Score: {score} </p> : null }
+      { loading && <p>Loading Questions...</p> }
+      { !loading && !gameOver && (
+        <TriviaCard 
+          questionNumber = {number + 1}
+          totalQuestions = {TOTAL_QUESTIONS}
+          question = {questions[number].question}
+          answers = {questions[number].answers}
+          userAnswer = {userAnswers ? userAnswers[number]: undefined}
+          callback = {checkAnswer}
+        /> 
+        )}
+      { !gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+        ) : null 
+      }
     </div>
   );
 }
--- src/services/QUIZ_API.ts
+++ src/services/QUIZ_API.ts
@@ -1,3 +1,22 @@
+import { StringLiteral } from "typescript";
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionExtended = Question & { answers: string[]}
+
+export type AnswerObject = {
+    question: string;
+    answer: string;
+    correct: boolean;
+    correctAnswer: string;
+}
 
 export enum Difficulty {
     EASY = "easy",
@@ -13,5 +32,13 @@ export enum QuizType {
 export const fetchTriviaQuestions = async (amount: number, difficulty: Difficulty, type: QuizType) => {
     const api = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=${type}`;
     const dataFromApi = await (await fetch(api)).json();
-    console.log(dataFromApi);
+    return dataFromApi.results.map((question: Question) => (
+        {
+            ...question,
+            answers: [
+                ...question.incorrect_answers,
+                question.correct_answer
+            ].sort().reverse()
+        }
+    ))
 }
