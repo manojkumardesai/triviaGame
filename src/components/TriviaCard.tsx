9e91c672a6dace434e4a2f57eb34fd5d0c0f31e8
manojkumardesai <manojkumardbe@gmail.com>
2020-12-09 03:01:54 +0530

    trivia card add structure and prop types

--- src/components/TriviaCard.tsx
+++ src/components/TriviaCard.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
-const TriviaCard = () => <div> Trivia Card </div>
+type CardProps = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNumber: number;
+    totalQuestions: number;
+}
+
+const TriviaCard : React.FC<CardProps> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNumber,
+    totalQuestions
+}) => (
+    <div>
+        <p className="number">
+            Question: {questionNumber} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML = {{ __html: question}} />
+        <div>
+            {answers.map((answer) => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer}} />
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>
+);
 
 export default TriviaCard;
