f45bc3a662e4dd0a1aea6a9c1541337b89d88617
manojkumardesai <manojkumardbe@gmail.com>
2020-12-09 02:51:34 +0530

    initial scaffolding of methods and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-function App() {
+// Components
+import TriviaCard from './components/TriviaCard';
+const App = () => {
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Trivia Game
+      <h1>Trivia Game</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">
+        Score:
+      </p>
+      <p>Loading Questions...</p>
+      <TriviaCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
---
+++ src/components/TriviaCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TriviaCard = () => <div> Trivia Card </div>
+
+export default TriviaCard;
